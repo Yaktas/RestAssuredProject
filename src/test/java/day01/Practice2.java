d0b8e1b41a0a226a50f7523a44a568a87a68fd68
Yaktas <joswhitestone@gmail.com>
2020-08-18 15:56:39 -0500

    adding files

--- src/test/java/day01/Practice2.java
+++ src/test/java/day01/Practice2.java
@@ -1,5 +1,9 @@
 package day01;
+import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
+
+import java.util.Arrays;
+import java.util.List;
 
 import static org.hamcrest.MatcherAssert.assertThat;
 import static org.hamcrest.Matchers.*;
@@ -30,6 +34,7 @@ public class Practice2 {
         //  is( some value )
         // equalTo( some value)
         //  or optionally   is ( equalTo(some value) )
+        // not
 
         assertThat( num1 + num2 ,  is(9)   );
         assertThat( num1 + num2 ,  equalTo(9)   );
@@ -59,6 +64,30 @@ public class Practice2 {
         assertThat(firstName , equalToIgnoringCase("kaisar ") );
         // how to ignore all whitespaces
         assertThat(firstName, equalToCompressingWhiteSpace("Kaisar"));
+
+
+    }
+
+    @DisplayName("Support for collection")
+    @Test
+    public void test2(){
+
+        List<Integer> numList = Arrays.asList(11,44,3,55,88,5) ;
+        // checking the list size is 6
+        assertThat(numList, hasSize(6)  );
+        // checking the list contains 11
+        assertThat(numList, hasItem(11) );
+
+        // has items is used to check multiple items : 11, 3, 5
+        assertThat(numList, hasItems(11,3,5));
+
+        // contains method works like equals here
+        // checking the list contains all the items in exact order : 11,44,3,55,88,5
+        assertThat(numList, contains(11,44,3,55,88,5 )   );
+
+        // checking the list contains all the items in any order  : 11, 44, 55,3,88
+        assertThat(numList,  containsInAnyOrder(11, 44, 55,3,88,5 )   );
+
 
     }
 
