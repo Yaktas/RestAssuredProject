d1aaf47e41d0be3c4afcc8e633315a04686c0caa
Yaktas <joswhitestone@gmail.com>
2020-08-20 10:11:36 -0500

    hello

--- src/test/java/day01/RestAssuredMethodChaining.java
+++ src/test/java/day01/RestAssuredMethodChaining.java
@@ -42,7 +42,7 @@ public class RestAssuredMethodChaining {
                 statusCode( is(200) ).        // asserting status code is 200
                 header("Content-Length",  equalTo("17") ).
                 header("Content-Type", is("text/plain;charset=UTF-8") ).
-                body( is("Hello from Sparta!") );
+                body( is("Hello from Sparta") );
         ;  // asserting header is "17"
         ;
 
@@ -61,13 +61,14 @@ public class RestAssuredMethodChaining {
         // you can chain multiple assertions
         // if any assertions fail , whole test fail.
 
-        given().  // add all your request specific information like header, query param, path var, body
-                header("Accept","application/json").
+        given().
+                header("Accept","application/xml").
                 when().
                 get("http://54.174.216.245:8000/api/spartans").
                 prettyPeek().
                 then().
-                statusCode( is(200) ) ;
+                statusCode( is(200) )
+        ;
 
     }
 
@@ -77,5 +78,4 @@ public class RestAssuredMethodChaining {
 
 
 }
-
 
