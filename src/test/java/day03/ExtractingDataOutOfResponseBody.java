0ed322a543bd1eec488a6f83c38cb86eb36a929d
Yaktas <joswhitestone@gmail.com>
2020-08-21 10:08:35 -0500

    day3

---
+++ src/test/java/day03/ExtractingDataOutOfResponseBody.java
@@ -0,0 +1,17 @@
+package day03;
+
+import org.junit.jupiter.api.DisplayName;
+import org.junit.jupiter.api.Test;
+
+public class ExtractingDataOutOfResponseBody {
+
+    @DisplayName("Getting Movie info")
+    @Test
+    public void test1(){
+
+
+
+    }
+
+
+}
