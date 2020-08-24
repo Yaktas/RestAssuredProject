01a15919e01f6ca4892705fc790472337ef51d76
Yaktas <joswhitestone@gmail.com>
2020-08-24 14:11:15 -0500

    day4

--- src/test/java/day02/SpartanTest2.java
+++ src/test/java/day02/SpartanTest2.java
@@ -5,9 +5,9 @@ import org.junit.jupiter.api.BeforeAll;
 import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
 
-import static io.restassured.RestAssured.given;
+import static io.restassured.RestAssured.*;
 import static org.hamcrest.Matchers.equalTo;
-import static org.hamcrest.Matchers.is;
+import static org.hamcrest.Matchers.*;
 
 public class SpartanTest2 {
 
--- src/test/java/day03/ExtractingDataOutOfResponseBody.java
+++ src/test/java/day03/ExtractingDataOutOfResponseBody.java
@@ -1,10 +1,9 @@
 package day03;
-
 import io.restassured.response.Response;
 import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
 
-import static io.restassured.RestAssured.*;
+import static io.restassured.RestAssured.given;
 
 public class ExtractingDataOutOfResponseBody {
 
@@ -18,12 +17,14 @@ public class ExtractingDataOutOfResponseBody {
         // t   = Boss Baby
         // Save the response into response object
 
-        Response response = given()
-                .baseUri("http://www.omdbapi.com")
-                .queryParam("apikey","26aa5b74")
-                .queryParam("t","Boss Baby").
+        Response response =
+                given()
+                        .log().all()
+                        .baseUri("http://www.omdbapi.com")
+                        .queryParam("apikey","26aa5b74")
+                        .queryParam("t","Boss Baby").
                         when()
-                .get();
+                        .get();
 
         //response.prettyPrint() ;
         System.out.println(  response.statusCode()  );
@@ -45,7 +46,6 @@ public class ExtractingDataOutOfResponseBody {
         // the jsonPath for first Rating source is Ratings[0].Source
         String firstRatingSrc = response.path("Ratings[0].Source");
         System.out.println("firstRatingSrc = " + firstRatingSrc);
-
 
 
 
