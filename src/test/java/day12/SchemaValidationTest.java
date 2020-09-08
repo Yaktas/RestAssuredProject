4ef774224511bd271004dd06a7fbcd665c9435b1
Yaktas <joswhitestone@gmail.com>
2020-09-08 10:04:36 -0500

    day11_12

---
+++ src/test/java/day12/SchemaValidationTest.java
@@ -0,0 +1,48 @@
+package day12;
+
+import io.restassured.RestAssured;
+import org.junit.jupiter.api.BeforeAll;
+import org.junit.jupiter.api.DisplayName;
+import org.junit.jupiter.api.Test;
+import utility.ConfigurationReader;
+
+import static io.restassured.RestAssured.*;
+import static io.restassured.module.jsv.JsonSchemaValidator.*;
+
+public class SchemaValidationTest {
+
+    // LETS JUMP RIGHT IN !!!
+    @BeforeAll
+    public static void init() {
+        // spartan1.base_url=http://54.174.216.245:8000
+        RestAssured.baseURI = ConfigurationReader.getProperty("spartan1.base_url");
+        RestAssured.basePath = "/api";
+    }
+
+    @DisplayName("Testing GET /Spartans response against Schema")
+    @Test
+    public void testAllSpartansSchema(){
+
+        when()
+                .get("/spartans").
+                then()
+                .body(matchesJsonSchemaInClasspath("AllSpartansSchema.json") ) ;
+
+    }
+
+    @DisplayName("Testing GET /Spartans/search response against Schema")
+    @Test
+    public void testSearchSpartansSchema(){
+
+        // search female in query param and validate response against schema
+        given()
+                .queryParam("gender","Female").
+                when()
+                .get("/spartans/search").
+                then()
+                .body(matchesJsonSchemaInClasspath("SearchSchema.json") ) ;
+
+    }
+
+}
+
